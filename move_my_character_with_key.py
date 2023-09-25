b62cae5f9b1f19969be23f8120520d9ec64749e3
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-09-25 22:00:33 +0900

    수많은 오류를 겪고,,,
    1. 리소스를 새로운 리소스로 변경함
    (프레임크기가 고정돼있지않아서 프레임을 자르기위한 리스트를 만들어 주었음)
    2. key up이 되지않던 오류 수정
    3. 함수를 빼고 그냥 while문안에 넣어주게 되었음 (오류 찾느라...)
    4. 걸어다닐땐 8방향으로 걸어다닐 수 있고
    스페이스바를 누르면 기존 프레임 크기와 다른 크기의 프레임의 이미지로 바꾸며 4방향으로 이동함. (활잡는 8방향 리소스를 못찾음 ㅠㅠ)
    5. 범위를 벗어나지않게 조정해줌

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,25 +1,38 @@
 from pico2d import *
 
-stand_ysize = ((0,118),(118,115),(233,119),(352,124),(476,114))
-walk_ysize = ((0,121),(121,117),(238,144),(382,120),(502,120))
-vh = ('v', 'h')
+cut = ((1,32,48),(4,32,48),(4,100,100))
+cs = (3,2,1,0,3,2,1,0)
 
-def handle_event():
-    global xdir, ydir
-    global state  #0 : stand 1: walk 2:run
-    global dir #0: up, 1:upleft 2:left 3:downleft 4:down
-    global vhdir
-    global  leftdown, rightdown, updown, downdown , spacedown
+open_canvas(800,600)
+back = load_image('TUK_GROUND.png')
+boy = load_image('boy.png')
 
+x = 400
+y = 300
+xdir = 0
+ydir =0
+state = 0  # 0 : stand 1: walk 2:run
+see = 0
+leftdown = 0; rightdown=0 ;updown=0;downdown=0;spacedown =0
+frame = 0
+
+
+while(True):
+    clear_canvas()
+    back.draw(400,300,800,600)
+    if (state < 2):boy.clip_composite_draw((cut[state][1]*(frame%cut[state][0])),400+(7-see)*cut[state][2],cut[state][1],cut[state][2],0,'n',x,y,2*cut[state][1],2*cut[state][2])
+    else : boy.clip_composite_draw((cut[state][1]*(frame%cut[state][0])),(cs[see])*cut[state][2],cut[state][1],cut[state][2],0,'n',x,y,2*cut[state][1],2*cut[state][2])
+
+    update_canvas()
+    frame = frame + 1
+    delay(0.05)
     events = get_events()
     for event in events:
         if (event.type == SDL_KEYDOWN):
             if (event.key == SDLK_RIGHT):
-                vhdir = 1
                 rightdown = 1
                 xdir += 1
             if (event.key == SDLK_LEFT):
-                vhdir = 0
                 leftdown = 1
                 xdir -= 1
             if (event.key == SDLK_UP):
@@ -29,44 +42,51 @@ def handle_event():
                 downdown = 1
                 ydir -= 1
             if (event.key == SDLK_SPACE):
-                spacedown =1
+                spacedown = 1
         elif (event.type == SDL_KEYUP):
-            if (event.type == SDL_KEYDOWN):
-                if (event.key == SDLK_RIGHT):
-                    rightdown = 0
-                    xdir -= 1
-                if (event.key == SDLK_LEFT):
-                    leftdown = 0
-                    xdir += 1
-                if (event.key == SDLK_UP):
-                    updown = 0
-                    ydir -= 1
-                if (event.key == SDLK_DOWN):
-                    downdown = 0
-                    ydir += 1
-                if (event.key == SDLK_SPACE):
-                    spacedown = 0
-    if (spacedown and (updown or downdown or rightdown or leftdown)): state = 2
-    elif (updown or downdown or rightdown or leftdown): state = 1
-    else: state = 0
+            if (event.key == SDLK_RIGHT):
+                print("오른쪽키업!")
+                rightdown = 0
+                xdir -= 1
+            if (event.key == SDLK_LEFT):
+                leftdown = 0
+                xdir += 1
+            if (event.key == SDLK_UP):
+                updown = 0
+                ydir -= 1
+            if (event.key == SDLK_DOWN):
+                downdown = 0
+                ydir += 1
+            if (event.key == SDLK_SPACE):
+                spacedown = 0
+    if (spacedown and (updown or downdown or rightdown or leftdown)):
+        state = 2
+    elif (updown or downdown or rightdown or leftdown):
+        state = 1
+    else:
+        state = 0
 
-    if (updown and (leftdown or rightdown)):dir = 1
-    elif (downdown and (leftdown or rightdown)): dir = 3
-    elif ((leftdown or rightdown)): dir = 2
-    elif (updown): dir = 0
-    elif (downdown):dir = 4
+    if (updown and leftdown):
+        see = 5
+    elif (updown and rightdown):
+        see = 7
+    elif (downdown and leftdown):
+        see = 4
+    elif (downdown and rightdown):
+        see = 6
+    elif (leftdown):
+        see = 1
+    elif (rightdown):
+        see = 2
+    elif (updown):
+        see = 3
+    elif (downdown):
+        see = 0
 
-back = load_image('TUK_GROUND.png')
-stand = load_image('stand.png')
-walk = load_image('walk.png')
+    x += xdir * 5 * state
+    if (x>800): x = 800
+    if (x < 0 ): x = 0
+    y += ydir * 5 * state
+    if (y>600): y = 600
+    if (y < 0): y = 0
 
-xdir = 0
-ydir =0
-state = 0  # 0 : stand 1: walk 2:run
-dir = 4  # 0: up, 1:upleft 2:left 3:downleft 4:down
-vhdir = 0
-leftdown, rightdown, updown, downdown , spacedown
-
-open_canvas()
-
-while(True):
